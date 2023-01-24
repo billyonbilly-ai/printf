be6903ecd44fd3bcc03458f142eab83fb6870628
GeneralRad <eneojo990@gmail.com>
2023-01-24 08:10:20 -0800

    printf

--- print_oct.c
+++ print_oct.c
@@ -3,8 +3,8 @@
  * print_oct - prints decimal number in octal
  * @arguments: input number
  * @buf: buffer pointer
- * @ibuf: index for buffer pointer
- * Return: number of chars printed.
+  @ibuf: index for buffer pointer
+ * Return: Number of chars printed.
  */
 int print_oct(va_list arguments, char *buf, unsigned int ibuf)
 {
